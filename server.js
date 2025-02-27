4e7f099f39e750276086fdf0dd5377226f209b57
Rogergrosado <rgrosado1@gmail.com>
2025-02-26 21:50:43 -0400

    Added .env file and updated configuration

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('ProfitPath API Running'));
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("ProfitPath Backend Running");
+});
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
